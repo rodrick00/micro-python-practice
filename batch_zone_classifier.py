8583c2a29c7c94e949a005a8b0b821cf68b10d7d
Kashaf Shaikh <rodrick1137@gmail.com>
2026-06-12 22:40:56 +0530

    Create batch_zone_classifier.py

---
+++ batch_zone_classifier.py
@@ -0,0 +1,14 @@
+from zone_classifier.py import classify_zone
+def main():
+    full_data = {'Ampicillin': 12, 'Tetracycline': 17}
+    final_result = classify_all(full_data)
+    print(final_result)
+def classify_all(zone_dict) :
+    new_dict = {}
+    for every_antibiotic in zone_dict:
+        size_of_zone = zone_dict[every_antibiotic]
+        classification_of_antibiotic = classify_zone(size_of_zone)
+        new_dict[every_antibiotic] = classification_of_antibiotic
+    return new_dict
+if __name__ == "__main__":
+    main()
